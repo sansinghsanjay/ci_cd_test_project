4772c784bc03e0ddd4ed28f40992750df4723122
sansinghsanjay <san.singhsanjay@gmail.com>
2023-04-04 21:40:43 +0530

    added a basic Django project

--- db/sql_script.sql
+++
@@ -1,6 +0,0 @@
-create table user_record (
-	roll_no int primary key,
-	first_name varchar(30) not null,
-	last_name varchar(30) not null
-);
-insert into user_record (roll_no, first_name, last_name) values (1, 'Sanjay', 'Singh');
--- django_app/homeapp/urls.py
+++
@@ -1,8 +0,0 @@
-# packages
-from django.urls import path
-from .views import homeView, showRecordFun
-
-urlpatterns = [
-    path("", homeView, name="home page"),
-    path("show_record", showRecordFun, name="show record function"),
-]
